3d89dace0a1b257d486377e26cab3ccec44fe3ee
A1cey <mschelkle05@web.de>
2025-12-10 18:32:23 +0100

    Day 6 fixed benchmark

--- benches/day6.rs
+++ benches/day6.rs
@@ -4,9 +4,15 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use advent_of_code_2025::day6;
 
 fn solutions(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Day 6 Solutions");
+    let mut group = c.benchmark_group("Day 6 Solutions Part One");
 
-    group.bench_function("day6 solution1", |b| b.iter(|| day6::solution1::solve()));
+    group.bench_function("day6 part_one_iterator", |b| b.iter(|| day6::part_one_iterator::solve()));
+
+    group.finish();
+    
+    let mut group = c.benchmark_group("Day 6 Solutions Part Two");
+
+    group.bench_function("day6 part_two", |b| b.iter(|| day6::part_two::solve()));
 
     group.finish();
 }
